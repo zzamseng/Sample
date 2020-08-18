c2d8593aceccaf13aad82a54a519ede415f9b80a
shjung <zzamseng@gmail.com>
2020-08-18 23:19:16 +0900

    add: functionalbuilder

---
+++ C#/DesignPattern/BuilderSample/BuilderSample/FluentBuilderTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace BuilderSample
+{
+    public class Animal<T>
+        where T:Animal<T>
+    {
+
+        public string Type { get; set; }
+        public int Age { get; set; }
+
+        public T SetType(string type)
+        {
+            this.Type = type;
+            return (T)this;
+        }
+
+        public T SetAge(int age)
+        {
+            this.Age = age;
+            return (T)this;
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {Type}, Age:{Age} ";
+        }
+    }
+
+    public class Human<T>
+        : Animal<Human<T>>
+        where T:Human<T>
+    {
+        public string Name { get; set; }
+
+        public T SetName(string name)
+        {
+            this.Name = name;
+            return (T)this;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"name: {Name}";
+        }
+    }
+
+
+    public class AnimalBuilder
+    {
+        public class Builder : Human<Builder> { };
+        public static Builder New => new Builder();
+    }
+}
