6ae8b08a8b7f01cde692240bde75a4f02468e2c0
shjung <zzamseng@gmail.com>
2020-09-06 18:43:37 +0900

    feat: factory pattern sample init commit

---
+++ C#/DesignPattern/FactorySample/FactorySample/Factory.cs
@@ -0,0 +1,27 @@
+﻿  using System;
+namespace BuilderSample.FactoryPattern
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Factory
+    {
+        private static int _count = 0;
+
+        public Factory()
+        {
+
+        }
+
+        public Person CreatePerson(string name)
+        {
+            return new Person()
+            {
+                Id = _count++,
+            };
+        }
+    }
+}
---
+++ C#/DesignPattern/FactorySample/FactorySample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BuilderSample.FactoryPattern;
+
+namespace FactorySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Factory().CreatePerson("test");
+
+            Console.WriteLine(person);
+        }
+    }
+}
