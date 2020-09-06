93b739ad6ba3f917dcb15562cf748848d5a8f5f7
shjung <zzamseng@gmail.com>
2020-09-06 19:56:30 +0900

    feat: prototype pattern init commit

---
+++ C#/DesignPattern/ProtoTypeSample/ProtoTypeSample/Program.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+
+namespace ProtoTypeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Line line = new Line();
+            line.Start = new Point() {X=1, Y=2 };
+            line.End = new Point() { X = 3, Y = 4 };
+
+            Console.WriteLine(line);
+
+            var tt = line.DeepCopy();
+            var shalow = line;
+            shalow.Start.X = 10;
+            tt.Start.X = 15;
+
+            Console.WriteLine(line);
+            Console.WriteLine(shalow);
+            Console.WriteLine(tt);
+        }
+    }
+}
---
+++ C#/DesignPattern/ProtoTypeSample/ProtoTypeSample/Prototype.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ProtoTypeSample
+{
+    public static class DeepCopyExtention
+    {
+        public static T DeepCopy<T>(this T self)
+        {
+            using(var ms = new MemoryStream())
+            {
+                var x = new XmlSerializer(typeof(T));
+                x.Serialize(ms, self);
+                ms.Position = 0;
+
+                var tmp = x.Deserialize(ms);
+
+                return (T)tmp;
+            }
+        }
+    }
+
+
+    public class Point
+    {
+        public int X, Y;
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            return this.DeepCopy<Line>();
+        }
+
+        public override string ToString()
+        {
+            return $"{Start.X} / {Start.Y} ,, {End.X} / {End.Y}";
+        }
+    }
+}
